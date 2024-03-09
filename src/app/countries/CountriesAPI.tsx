b13841df565be0e6f9bfe820f8d5e3bb1c494205
Martin Holland <martin.holland@free.fi>
2024-03-09 21:33:07 +0200

    end of lesson commit

---
+++ src/app/countries/CountriesAPI.tsx
@@ -0,0 +1,74 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+
+interface CountryDetails {
+  capital: string[];
+  name: CountryInfo;
+}
+
+interface CountryInfo {
+  common: string;
+  official: string;
+  nativeName: NativeNames;
+}
+
+interface NativeNames {
+  [key: string]: LanguageDetails;
+}
+
+interface LanguageDetails {
+  official: string;
+  common: string;
+}
+
+const CountriesAPI = () => {
+  const [countries, setCountries] = useState<CountryDetails[]>([]);
+  //   const [countries, setCountries] = useState([]);
+
+  const getCountriesInfo = async () => {
+    const response = await axios.get(
+      //   "https://restcountries.com/v3.1/all?fields=capital"
+      "https://restcountries.com/v3.1/all?fields=name,capital"
+    );
+    console.log(response.data);
+    if (response.data) {
+      setCountries(response.data);
+    }
+  };
+
+  useEffect(() => {
+    getCountriesInfo();
+  }, []);
+
+  return (
+    <>
+      <div>Countries API will be here</div>
+      {countries.map((country, index) => (
+        <Card
+          key={`${country.capital}_${index}`}
+          style={{ maxWidth: "20rem", height: "10rem" }}
+        >
+          <div key={`${country.capital}_${index}`}>
+            <h2>{country.capital[0]}</h2>
+            <p>Country Name: {country.name.official}</p>
+            {/* Edit the below to your own data */}
+            <p>{}</p>
+            {Object.entries(country.name.nativeName).map(([key, value]) => (
+              <div key={key}>
+                <p key={key}>
+                  {key}: {value.common}
+                </p>
+                <p>{value.common}</p>
+                <p>{value.official}</p>
+              </div>
+            ))}
+          </div>
+        </Card>
+      ))}
+    </>
+  );
+};
+
+export default CountriesAPI;
